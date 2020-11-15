c1cf91f5673d49ad1f0537517d28b69402d22426
oguzhan.posul <oguzhan.posul@trendyol.com>
2020-11-16 00:05:18 +0300

    test

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
 
 export const App: React.FC = () => {
-  const x: any = "123";
-  console.log(x);
-  
-  return (
-    <p>I Love Parcel</p>
-  );
-}
+    const x: any = '123';
+    console.log(x);
+
+    return <p>I Love Parcel</p>;
+};
--- client/server.tsx
+++ client/server.tsx
@@ -1,10 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import "./styles/index.less";
+import './styles/index.less';
 import { App } from './components/App';
 
-ReactDOM.render(
-  <App  />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
