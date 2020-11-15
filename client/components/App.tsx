5733dc4195df9564a291eb38e163f305c78cfdb3
oguzhan.posul <oguzhan.posul@trendyol.com>
2020-11-16 00:12:40 +0300

    try pretty

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 
 export const App: React.FC = () => {
     const x: any = '123';
-    console.log(x);
+    const arr = [];
+    console.log(x, arr);
 
     return <p>I Love Parcel</p>;
 };
