a3faaed29b09bd18929eacfb6a8d17c1f9db7550
oguzhan.posul <oguzhan.posul@trendyol.com>
2020-11-15 22:40:25 +0300

    init project

---
+++ client/components/App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export const App: React.FC = () => {
+  return (
+    <p>I Love Parcel</p>
+  );
+}
---
+++ client/server.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import "./styles/index.less";
+import { App } from './components/App';
+
+ReactDOM.render(
+  <App  />,
+  document.getElementById('root')
+);
